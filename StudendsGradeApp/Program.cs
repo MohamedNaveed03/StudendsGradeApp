ce9a9362891e4268de301b46181036365f3efd4f
Mohamed Naveed <naveedmohamed870@gmail.com>
2024-10-23 00:57:20 +0530

    added sqlite in azure to take the database

--- StudendsGradeApp/Program.cs
+++ StudendsGradeApp/Program.cs
@@ -1,13 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using StudendsGradeApp.Data;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Set the database path based on the environment
+string dbPath;
+if (builder.Environment.IsDevelopment())
+{
+    // Local development path
+    dbPath = @"C:\Users\navee\source\repos\StudendsGradeApp\StudendsGradeApp\StudentDB.db";
+}
+else
+{
+    // Azure or production path
+    dbPath = Path.Combine(Environment.CurrentDirectory, "StudentDB.db");
+}
+
+// Update the connection string to use the correct database path
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration["ConnectionStrings:localDb"] = $"Data Source={dbPath}";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(
-    builder.Configuration.GetConnectionString("localDb")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("localDb")));
 
 var app = builder.Build();
 
